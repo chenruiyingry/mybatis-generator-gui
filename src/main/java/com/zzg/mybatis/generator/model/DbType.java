cade14ea91a365c883f56e68b80b8dd0a965e782
ryan <chenruiying1997@foxmail.com>
2024-03-20 10:45:01 +0800

    fix m3 error

--- src/main/java/com/zzg/mybatis/generator/model/DbType.java
+++ src/main/java/com/zzg/mybatis/generator/model/DbType.java
@@ -10,7 +10,7 @@ public enum DbType {
     Oracle("oracle.jdbc.OracleDriver", "jdbc:oracle:thin:@//%s:%s/%s", "ojdbc6.jar"),
     PostgreSQL("org.postgresql.Driver", "jdbc:postgresql://%s:%s/%s", "postgresql-9.4.1209.jar"),
 	SQL_Server("com.microsoft.sqlserver.jdbc.SQLServerDriver", "jdbc:sqlserver://%s:%s;databaseName=%s", "sqljdbc4-4.0.jar"),
-	Sqlite("org.sqlite.JDBC", "jdbc:sqlite:%s", "sqlite-jdbc-3.19.3.jar");
+	Sqlite("org.sqlite.JDBC", "jdbc:sqlite:%s", "sqlite-jdbc-3.7.15-M1.jar");
 
     private final String driverClass;
     private final String connectionUrlPattern;
